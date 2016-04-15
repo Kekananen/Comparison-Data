edeb3eac13115cf6a4e7ef5ba8089ef6abf9988f
Kekananen <Kekananen@gmail.com>
2016-04-15 18:05:42 -0500

    Added files via upload

---
+++ GeographicalCoordinates.py
@@ -0,0 +1,24 @@
+import sys, random
+
+# out = open(sys.argv[1], "w")
+
+GeographicalCoords = matrix(0, nrow =2344, ncol =2)
+
+# xCoords = []
+# yCoords = []
+# Coords
+# 
+# rangeX = (-100, 100)
+# rangeY = (-100, 100)
+# quantity = 2344
+# 
+# randPointsY = []
+# randPointsX = []
+# i = 0
+# while i<quantity:
+#     x = random.randrange(*rangeX)
+#     y = random.randrange(*rangeY)
+#     randPointsY.append(y)
+#     randPointsX.append(x)
+#     i += 1
+# print(randPointsX)
